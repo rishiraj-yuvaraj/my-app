baaaf2d55801d25caa68bc6215baf259e7c63973
rishiraj-yuvaraj <rraj39667@gmail.com>
2022-10-29 16:01:04 +0530

    Day 22

--- src/components/Bots/index.js
+++ src/components/Bots/index.js
@@ -13,7 +13,7 @@ const bots = ({products}) =>{
                     <div className="card-body">
                     <h5 className="card-title">{product.name}</h5>
                     <p className="card-text">{product.description}</p>
-                    <p>{product.Price}</p>
+                    <p>Price:{product.Price}</p>
                     <a href="#" className="btn btn-primary">Buy Now</a>
                     </div>
                 </div>
